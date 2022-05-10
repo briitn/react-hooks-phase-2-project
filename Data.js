96733c6a50fb8e818bb1bfd4ffe01868d8747490
Prince Gobah <53085565+briitn@users.noreply.github.com>
2022-05-10 03:19:32 -0400

    Added data

---
+++ Data.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+const countryList = ['Algeria','Angola','Benin','Botswana','Burkina Faso','Burundi','Cabo Verde','Cameroon','Central African Republic','Chad','Comoros','Democratic Republic of the Congo','Republic of the Congo','Cote dIvoire','Djibouti','Egypt','Equatorial Guinea','Eritrea','Ethiopia','Gabon','Gambia','Ghana','Guinea','Guinea Bissau','Kenya','Lesotho','Liberia','Libya','Madagascar','Malawi','Mali','Mauritania','Mauritius','Morocco','Mozambique','Namibia','Niger','Nigeria','Rwanda','Sao Tome and Principe','Senegal','Seychelles','Sierra Leone','Somalia','South Africa','South Sudan','Sudan','Swaziland','Tanzania','Togo','Tunisia','Uganda','Zambia','Zimbabwe'];
+
+const africa=[
+    {
+"country": "Ghana",
+"image": [
+  "https://images.prismic.io/clacified/d1c4d065-21fb-46ae-a2c7-70016c28f1a4_images+%2832%29.jpeg?auto=compress%2Cformat&rect=101%2C0%2C644%2C362&w=480&h=270",
+  "https://images.prismic.io/clacified/dd02456b-2897-4b6a-a5f6-c95f5581c34a_images+%2819%29.jpeg?auto=compress%2Cformat&rect=0%2C0%2C738%2C415&w=320&h=180"
+],
+"alt": [
+  "Kakum National Park",
+  "Cape Coast"
+],
+"flag": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNKgTKHlPUAnR7GZjvk2fbraz06ZxwNLEabA&usqp=CAU",
+    },
+{
+"id": 4,
+"country": "Kenya",
+"image": [
+  "https://www.planetware.com/photos-large/KEN/kenya-Maasai-mara.jpg",
+  "https://www.planetware.com/photos-large/KEN/kenya-lake-nakuru-national-park.jpg"
+],
+"alt": [
+  "Maasai Mara National Reserve",
+  "Lake Nakuru National Park"
+],
+"flag": "https://media.istockphoto.com/vectors/kenya-vector-id1190603924?k=20&m=1190603924&s=612x612&w=0&h=2kR5QmctcIMtmq4vUzPHmaYY82ljPPBJBdg-g6bPP8o=",
+
+},
+{
+"id": 5,
+"country": "Nigeria",
+"image": [
+  "https://cdn.thecrazytourist.com/wp-content/uploads/2016/11/Abeokuta.jpg",
+  "https://cdn.thecrazytourist.com/wp-content/uploads/2016/11/Yankari-National-Park.jpg"
+],
+"alt": [
+  "Abeokuta",
+  "Yankari National Park"
+],
+"flag": "https://media.istockphoto.com/vectors/nigeria-vector-id652740802?k=20&m=652740802&s=612x612&w=0&h=wLWuHUBs2Xx30KaUP1sNq-zt8_WF6mVeRZWNS6BFdno=",
+
+},
+{
+"id": 6,
+"country": "Ethiopia",
+"image": [
+  "https://cdn.thecrazytourist.com/wp-content/uploads/2016/06/Babile-Elephant-Sanctuary-scaled.jpg",
+  "https://cdn.thecrazytourist.com/wp-content/uploads/2016/06/Lalibela.jpg"
+],
+"alt": [
+  "Babile Elephant Sanctuary",
+  "Lalibela"
+],
+"flag": "https://media.istockphoto.com/vectors/flag-of-ethiopia-vector-id693976100?k=20&m=693976100&s=612x612&w=0&h=wDVj9ipU7f2iKEcfrrFkW-XGrpm6pVOYbA9mkmaRU8M=",
+
+},
+{
+"id": 7,
+"country": "Madagascar",
+"image": [
+  "https://www.touropia.com/gfx/d/tourist-attractions-in-madagascar/masoala_national_park.jpg",
+  "https://www.touropia.com/gfx/d/tourist-attractions-in-madagascar/isalo_national_park.jpg"
+],
+"alt": [
+  "Masoala National Park",
+  "Isalo National Park"
+],
+"flag": "https://media.istockphoto.com/vectors/malagasy-flag-of-madagascar-vector-id1335529523?k=20&m=1335529523&s=612x612&w=0&h=3-CaJKyBcbm49acG0ZEqNb1AwIRaywCqBCQDGpQHw6w=",
+}]
+export {countryList, africa}
