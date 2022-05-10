2e8581e837f9e2e69cefe7ac865906e29d3fffa6
Prince Gobah <53085565+briitn@users.noreply.github.com>
2022-05-10 03:29:26 -0400

    useEffect fetch request

--- Pictures.js
+++ Pictures.js
@@ -5,10 +5,15 @@ import React, {useState, useEffect} from "react";
 
 function Pictures({world,country,setCountry}){
     const [cpArray, setCpArray]=useState([])
-   
+    useEffect(() => {
+        fetch("http://localhost:3000/prices")
+        .then((r)=>r.json())
+        .then((items)=>setCpArray(items))
+    },[])
+
+
+
 }
-
-
 
 
 export default Pictures
