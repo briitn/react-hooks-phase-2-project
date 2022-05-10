1d38bfb7093d8434a0cb36407f71c351ffc36ce0
Prince Gobah <53085565+briitn@users.noreply.github.com>
2022-05-10 03:24:11 -0400

    mapped items to display

---
+++ Tour.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+
+function Tour({africa, setCountry}){
+
+  const mapMapstuff=africa.map(item=>{
+    return (
+        <div key={item.country}>
+           <div>{item.country}</div>
+
+
+        
+          <div><img src={item.flag}
+
+        alt="flag"
+        width='50px'
+         />
+     
+         </div>
+       
+<div>
+        <img className="image1"
+        src={item.image[0]}
+        alt={item.alt[0]}
+        width={300}/>
+        <span className="tooltiptext">{item.alt[0]}</span>
+
+        <img className="image2"
+        src={item.image[1]}
+        alt={item.alt[1]}
+        width={300}/>
+         <span className="tooltiptext">{item.alt[1]}</span>
+        </div>
+        </div>
+    )
+  })
+    return (
+        <main>
+              
+            <h1>
+               <span className="see"> See </span><img src="https://image.shutterstock.com/image-vector/africa-continent-map-ornate-ethnic-260nw-1315315565.jpg"
+                alt="Africa map"
+                width={20}/><span className="frica">frica</span>
+            </h1>
+           
+             {mapMapstuff}   
+              
+
+        </main>
+    )
+}
+
+
+export default Tour
