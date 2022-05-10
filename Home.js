ec4d5b8d7b23e3d57afcd068ce52f4a1c838e781
Prince Gobah <53085565+briitn@users.noreply.github.com>
2022-05-10 03:09:02 -0400

    Added image to home page

---
+++ Home.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react"
+import { Link } from "react-router-dom"
+
+function Home(){
+    return <Fragment> <div className="Home">
+       
+        <img src="https://image.shutterstock.com/image-vector/africa-continent-map-ornate-ethnic-260nw-1315315565.jpg"
+
+        alt="Cultral African Map"
+        width='250px'
+        height={200} />
+    </div>
+    
+    </Fragment>
+}
+
+
+
+
+
+export default Home
