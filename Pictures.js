9ee2185194424a50ca5df80b5f617347803a98a5
Prince Gobah <53085565+briitn@users.noreply.github.com>
2022-05-10 03:27:45 -0400

    created array to hold data

---
+++ Pictures.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from "react";
+
+
+
+
+function Pictures({world,country,setCountry}){
+    const [cpArray, setCpArray]=useState([])
+   
+}
+
+
+
+
+export default Pictures
