f153ee6047f05fc207eb8dec41afdd4b388edd23
Prince Gobah <53085565+briitn@users.noreply.github.com>
2022-05-10 03:38:27 -0400

    added fetch request and setSubmit function

--- Pictures.js
+++ Pictures.js
@@ -18,12 +18,68 @@ function changeCountry(event){
 }
 
 
+
 const rng=Math.random()*1000
 const [bought,setBought]=useState()
 const [price,setPrice]=useState(Math.floor(rng))
 console.log(rng)
 const [isSubmit,setIsSubmit]=useState(false)
+function changeSubmit(e){
+    
+    e.preventDefault()
+    {fetch("http://localhost:3000/prices", 
+    {method:"POST", headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(
+        formData 
+      )}
+      
+    ).then((r)=>r.json())
+    .then((items)=>cpArray.push(items))
 
+    setIsSubmit(true)}
+    setBought(false)
+   
+    }
+
+ 
+  
+
+console.log(price)
+
+
+const formData={
+    "country":country,
+    "price":price
+}
+
+
+function postThis(){
+    
+setBought(true)
+}
+
+
+
+    
+
+    return <div>
+
+
+   <form >
+       <input
+       onChange={changeCountry}
+       type='text'
+       placeholder="country"
+       value={country}
+       />
+       <button onClick={changeSubmit} >submit</button>
+   </form>
+   
+     
+
+    </div>
 }
 
 
